3d705276326879aa795cb00ecd0c5850ba9fbd74
clecat <clecat@student.42nice.fr>
2023-01-17 12:21:44 +0100

    gestion dollar

--- Parse/dollar_var.c
+++ Parse/dollar_var.c
@@ -6,13 +6,13 @@
 /*   By: clecat <clecat@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 11:43:29 by clecat            #+#    #+#             */
-/*   Updated: 2023/01/16 17:24:37 by clecat           ###   ########.fr       */
+/*   Updated: 2023/01/17 11:35:51 by clecat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-
+//recupere la valeur de la variable
 char	*recup_valvar(char *str)
 {
 	char	*tmp;
@@ -70,7 +70,53 @@ void	rm_dollarvar(char *name)
 		end[j] = '\0';
 		free(g_mini.line);
 		g_mini.line = ft_strjoin(start, end);
+		free(start);
+		free(end);
 	}
+}
+
+void	change_linevar(char *str)
+{
+	char	*tmp;
+	char	*start;
+	char	*end;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	start = malloc(sizeof(char) * ft_strlen(g_mini.line));
+	while (g_mini.line[i] != '$')
+	{
+		start[i] = g_mini.line[i];
+		i++;
+	}
+	start[i] = '\0';
+	tmp = ft_strjoin(start, str);
+	free(start);
+	while (g_mini.line[i] != ' '  && g_mini.line[i] != '\0')
+		i++;
+	if(i == ft_strlen(g_mini.line))
+	{
+		free(g_mini.line);
+		g_mini.line = ft_strdup(tmp);
+	}
+	else
+	{
+		end = malloc(sizeof(char) * ft_strlen(g_mini.line));
+		while (g_mini.line[i])
+		{
+			end[j] = g_mini.line[i];
+			j++;
+			i++;
+		}
+		end[j] = '\0';
+		free(g_mini.line);
+		g_mini.line = ft_strjoin(tmp, end);
+		free(tmp);
+		free(end);
+	}
+	printf("line = %s\n", g_mini.line);
 }
 
 //gestion dollar //segfault
@@ -87,10 +133,15 @@ void	ft_dollar(char *str)
 			break ;
 		i++;
 	}
+	free(tmp);
 	if (i == tablen(g_mini.c_env))
 		rm_dollarvar(str);
 	else
+	{
 		str = recup_valvar(g_mini.c_env[i]);
+		change_linevar(str);
+	}
+	free(str);
 }
 
 /*
