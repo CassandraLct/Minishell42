e08871034fe4868ea8d7e668e0e05d2e8b4283af
Raphael Di Marzio <rdi-marz@c2r4p6.42nice.fr>
2023-01-17 17:46:08 +0100

    removed the printf

--- Parse/splitcmd.c
+++ Parse/splitcmd.c
@@ -8,7 +8,6 @@ char	*remove_double_space(char *line)
 
 	i = ft_strlen(line) + 1;
 	resu = ft_calloc(i, sizeof(*resu));
-//	ft_bzero(resu, i);
 	i = 0;
 	j = 0;
 	while (line[i])
@@ -64,20 +63,15 @@ char	**pre_split(char *line)
 	resu = ft_calloc(i, sizeof(*resu));
 	index = 0;
 	j = ft_strlen(line) + 1;
-	printf("pre_split\n");
 	while (index < i - 1)
 	{
 		resu[index] = ft_calloc(j, sizeof(*resu[index]));
-//		ft_bzero((void *)resu[index], j);
-		printf("resu[%d]=[%s] ", index, resu[index]);
 		index++;
 	}
-	printf("\n");
 	resu[index] = NULL;
 	i = 0;
 	j = 0;
 	index = 0;
-	printf("pre_split suite\n");
 	while (line[i])
 	{
 		if (line[i] == '\'')
@@ -109,7 +103,6 @@ char	**pre_split(char *line)
 		else if (line[i] == ' ')
 		{
 			j = 0;
-			printf("space, resu[%d]=[%s]\n", index, resu[index]);
 			index++;
 		}
 		else
@@ -119,8 +112,6 @@ char	**pre_split(char *line)
 		}
 		i++;
 	}
-	printf("end, resu[%d]=[%s]\n", index, resu[index]);
-	printf("end, resu[%d]=[%s]\n", index + 1, resu[index + 1]);
 	return (resu);
 }
 
@@ -152,32 +143,19 @@ t_cmd	*alloc_cmd(char **list)
 
 	i = count_redir(list, '<');
 	j = count_redir(list, '>');
-	printf("i=[%d], j=[%d]\n", i, j);
 	resu = ft_calloc(1, sizeof(*resu));
 	resu->stdin = ft_calloc(2 * i + 1, sizeof(*resu->stdin));
 	resu->stdout = ft_calloc(2 * j + 1, sizeof(*resu->stdout));
 	resu->cmd = ft_calloc((tablen(list) - i - j + 1), sizeof(*resu->cmd));
-	// test
-	i = 0;
-	while (list[i])
-	{
-		printf("list[%d]=[%s] ", i, list[i]);
-		i++;
-	}
-	printf("list[%d]=[%s] \n", i,list[i]);
-	// end test
 	i = 0;
 	j = 0;
 	while (list[i])
 	{
-		printf("redir in, i=[%d], list[%d]=[%s], list[%d]=[%s] \n", i, i, list[i], i + 1, list[i + 1]);
 		if (list[i][0] == '<')
 		{
 			resu->stdin[j] = ft_strdup(list[i]);
 			resu->stdin[j + 1] = ft_strdup(list[i + 1]);
 			resu->stdin[j + 2] = NULL; // to remove if bzero ok
-			printf("list[%d]=[%s] list[%d]=[%s]\n", i, list[i], i + 1, list[i + 1]);
-			printf("resu->stdin[%d]=[%s] resu->stdin[%d]=[%s]\n", j, resu->stdin[j], j + 1, resu->stdin[j + 1]);
 			j = j + 2;
 			k = i;
 			while (list[k])
@@ -186,16 +164,6 @@ t_cmd	*alloc_cmd(char **list)
 				list[k] = list[k + 2]; // put 2 null at the end of list ?
 				k++;
 			}
-			// test
-			k = 0;
-			printf("---after removing redir in --->");
-			while (list[k])
-			{
-				printf("list[%d]=[%s] ", k, list[k]);
-				k++;
-			}
-			printf("list[%d]=[%s] \n", k, list[k]);
-			// end test
 			i--;
 		}
 		i++;
@@ -204,14 +172,11 @@ t_cmd	*alloc_cmd(char **list)
 	j = 0;
 	while (list[i])
 	{
-		printf("redir out, i=[%d]\n", i);
 		if (list[i][0] == '>')
 		{
 			resu->stdout[j] = ft_strdup(list[i]);
 			resu->stdout[j + 1] = ft_strdup(list[i + 1]);
 			resu->stdout[j + 2] = NULL; // to remove if bzero ok
-			printf("list[%d]=[%s] list[%d]=[%s]\n", i, list[i], i + 1, list[i + 1]);
-			printf("resu->stdout[%d]=[%s] resu->stdout[%d]=[%s]\n", j, resu->stdout[j], j + 1, resu->stdout[j + 1]);
 			j = j + 2;
 			k = i;
 			while (list[k])
@@ -220,16 +185,6 @@ t_cmd	*alloc_cmd(char **list)
 				list[k] = list[k + 2]; // put 2 null at the end of list ?
 				k++;
 			}
-			// test
-			k = 0;
-			printf("---after removing redir out --->");
-			while (list[k])
-			{
-				printf("list[%d]=[%s] ", k, list[k]);
-				k++;
-			}
-			printf("list[%d]=[%s] \n", k, list[k]);
-			// end test
 			i--;
 		}
 		i++;
@@ -240,12 +195,9 @@ t_cmd	*alloc_cmd(char **list)
 	{
 		resu->cmd[j] = ft_strdup(list[i]);
 		resu->cmd[j + 1] = NULL;
-		printf("list[%d]=[%s]\n", j, list[j]);
-		printf("resu->cmd[%d]=[%s]\n", j, resu->cmd[j]);
 		i++;
 		j++;
 	}
-	printf("end of alloc\n");
 	return (resu);
 }
 
@@ -259,14 +211,9 @@ t_cmd	*split_inst(char *temp)
 	resu = NULL;
 	i = ft_strlen(temp) + 1;
 	tmpclean = ft_calloc(i, sizeof(*tmpclean));
-	printf("size of tmpcleam=[%lu], i=[%d]\n", sizeof(tmpclean), i);
 	tmpclean = remove_double_space(temp);
-	printf("split_inst, after remove double space\n");
-	printf("tmpclean=[%s]\n", tmpclean);
 	list = pre_split(tmpclean);
-	printf("split_inst, after pre split\n");
 	resu = alloc_cmd(list);
-	printf("split_inst, after alloc_cmd\n");
 	return (resu);
 }
 
@@ -344,8 +291,6 @@ t_cmd	**spliter3(char **inst)
 	{
 		temp[i] = ft_strtrim(inst[i], " ");
 		tempclean[i] = ft_space_bracket(temp[i]);
-		printf("tempclean[%d]=%s\n", i, tempclean[i]);
-		// ajouter ici le traitement de < et > sans espace
 		i++;
 	}
 	temp[i] = NULL;
@@ -353,12 +298,10 @@ t_cmd	**spliter3(char **inst)
 	i = 0;
 	while (tempclean[i])
 	{
-		printf("spliter3, processing resu[%d]...\n", i);
 		resu[i] = split_inst(tempclean[i]);
 //		free(temp[i]);
 		i++;
 	}
 	resu[i] = NULL;
-	printf("spliter3, out of second while\n");
 	return (resu);
 }
