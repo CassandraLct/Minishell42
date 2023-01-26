3e7f84298607e4d67413fea2369f3b74a22afb06
clecat <clecat@student.42nice.fr>
2023-01-26 17:51:32 +0100

    planning

--- main.c
+++ main.c
@@ -6,45 +6,44 @@
 /*   By: clecat <clecat@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 16:25:38 by clecat            #+#    #+#             */
-/*   Updated: 2023/01/26 15:15:38 by clecat           ###   ########.fr       */
+/*   Updated: 2023/01/26 17:50:13 by clecat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 //grep >test < Makefile "minishell" | cat test
 
-int	heredoc(t_cmd **cmd)
-{
-	char	*line;
-	int		pp[2];
+// int	heredoc(t_cmd **cmd)
+// {
+// 	char	*line;
+// 	int		pp[2];
 	
-	pipe(pp);
-	//printf("stdin[1] = %s\n", cmd[0]->stdin[1]);
-	if (cmd[0]->stdin[0] == NULL)
-		return (1);
-	if (cmd[0]->stdin[0] != NULL && cmd[0]->stdin[1] == NULL) // segfault
-	{
-		printf("minishell: syntax error near unexpected token `newline'\n");
-		g_mini.ret_err = 258;
-		return (1);
-	}
-	else
-	{
-		while (1)
-		{
-			line = readline("> ");
-			if(ft_strncmp(cmd[0]->stdin[1], line, ft_strlen(line)) == 0)
-			{
-				free(line);
-				close(pp[1]);
-				return(pp[0]);
-			}
-			write(pp[1], line, ft_strlen(line));
-			write(pp[1], "\n", 1);
-			free(line);
-		}
-	}
-}
+// 	pipe(pp);
+// 	if (cmd[0]->stdin[0] == NULL)
+// 		return (1);
+// 	if (cmd[0]->stdin[0] != NULL && cmd[0]->stdin[1] == NULL) // segfault
+// 	{
+// 		printf("minishell: syntax error near unexpected token `newline'\n");
+// 		g_mini.ret_err = 258;
+// 		return (1);
+// 	}
+// 	else
+// 	{
+// 		while (1)
+// 		{
+// 			line = readline("> ");
+// 			if(ft_strncmp(cmd[0]->stdin[1], line, ft_strlen(line)) == 0)
+// 			{
+// 				free(line);
+// 				close(pp[1]);
+// 				return(pp[0]);
+// 			}
+// 			write(pp[1], line, ft_strlen(line));
+// 			write(pp[1], "\n", 1);
+// 			free(line);
+// 		}
+// 	}
+// }
 
 //modifier le split de la line
 int	main(int argc, char **argv, char **envp)
@@ -67,8 +66,8 @@ int	main(int argc, char **argv, char **envp)
 			if (g_mini.struct_cmd != NULL)
 			{
 				verif_struct_cmd(g_mini.struct_cmd);
-				new_filein = heredoc(g_mini.struct_cmd);
-				printf("filein = %d\n", new_filein);
+				// new_filein = heredoc(g_mini.struct_cmd);
+				// printf("filein = %d\n", new_filein);
 				redir_pipe(&g_mini, g_mini.struct_cmd);
 				free_t_cmd(g_mini.struct_cmd);
 			}
