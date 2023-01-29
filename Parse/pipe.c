32f69efa58d11cadce7583246400eab7415baa86
raphael from linux <rdmrdm3@gmail.com>
2023-01-29 11:56:42 +0100

    small change

--- Parse/pipe.c
+++ Parse/pipe.c
@@ -24,25 +24,43 @@ void	ft_exec2(t_min *mini, char **all_path, char **pathcmd, char **cmd)
 	}
 	else
 	{
-		ft_execve(mini, all_path, pathcmd, cmd);
-		free_tab(all_path);
-		free_tab(pathcmd);
+		dprintf(2, "child[%d] => no arg given to the command\n", i);
+	}
+	if (i != 0)
+	{
+		close(pp[i - 1][0]);
+		close(pp[i - 1][1]);
 	}
 }
 
 void	ft_set_pathexec2(t_min *mini, char **cmd)
 {
-	char	**all_path;
-	char	**pathcmd;
+	int		fdin;
+	int		fdout;
 
-	pathcmd = NULL;
-	pathcmd = init_cmd(cmd, pathcmd);
-	all_path = recup_path(mini);
-	if (all_path == NULL)
+//	waitpid(0, NULL, 0);
+	if (i == 0)
+		fdin = ft_redir_in2(cmd);
+	else
 	{
-		mini->ret_err = 127;
-		printf("minishell: %s: No such file or directory\n", cmd[0]);
-		free_tab(pathcmd);
+		fdin = pp[i - 1][0];
+	}
+	if (fdin)
+	{
+		dprintf(2, "parent => i=0, dup2(fdin, 0), fdin=[%d]\n", fdin);
+		dup2(fdin, 0);
+		close(fdin);
+	}
+	else
+	{
+		dprintf(2, "parent => no arg given to the command\n");
+	}
+
+	fdout = 1; // for testing
+	dprintf(2, "parent => after ft_redir_out, fdout=[%d]\n", fdout);
+	if (fdout == -1)
+	{
+		perror("redirection out :");
 		return ;
 	}
 <<<<<<< HEAD
@@ -50,26 +68,28 @@ void	ft_set_pathexec2(t_min *mini, char **cmd)
 =======
 	dup2(fdout, 1);
 	if (i != 0)
-	
+
 	{
 		close(pp[i - 1][0]);
 		close(pp[i - 1][1]);
 	}
 	close(pp[i][0]);
-	close(pp[i][1]);	
+	close(pp[i][1]);
 	dprintf(2, "parent => pp[%d][0]=[%d], pp[%d][1]=[%d] / ", i, pp[i][0], i, pp[i][1]);
 	dprintf(2, "parent => cmd[%d][0]=[%s] / end of parent\n", i, cmd[i]->cmd[0]);
+//	pid = fork();
+//	if (pid == 0)
 	ft_set_pathexec(&g_mini, cmd[i]->cmd);
 	exit (68);
->>>>>>> 72bf037 (change file1 for testing)
 }
 
 // main function to manage pipes
 int	piping(void)
 {
-	int		i;
-	pid_t	pid;
-	int		**pp;
+	int	i;
+	int	nbcmd;
+	int	pid;
+	int	**pp;
 
 	pp = ft_create_pipe(g_mini.struct_cmd);
 	i = 0;
