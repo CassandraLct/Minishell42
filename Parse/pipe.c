51b719ed7a319be2cc1ec493d9e67ce840a9c428
raphael from linux <rdmrdm3@gmail.com>
2023-01-29 12:24:26 +0100

    small change

--- Parse/pipe.c
+++ Parse/pipe.c
@@ -17,10 +17,118 @@ void	ft_exec2(t_min *mini, char **all_path, char **pathcmd, char **cmd)
 {
 	if (verif_cmd(all_path, pathcmd, cmd) != 0)
 	{
-		dprintf(2, "wrong command\n");
-		free_tab(all_path);
-		free_tab(pathcmd);
-		exit(1);
+		dprintf(2, "no redir in for cmd[%d]", nb);
+		return (-2);
+	}
+	if (pipe(pp) == -1)
+		return (-1);
+//	fdglobalin = dup(pp[1]);
+	fdglobalin = open("fileresu", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	i = 0;
+	while (cmd[nb]->stdin[i])
+	{
+		if (cmd[nb]->stdin[i + 1] == NULL)
+		{
+			dprintf(2, "redir in without file\n");
+			return (-3);
+		}
+		if (ft_strcmp(cmd[nb]->stdin[i], "<") == 0)
+		{
+			fd = open(cmd[nb]->stdin[i + 1], O_RDONLY);
+			if (fd == -1)
+			{
+				perror(cmd[nb]->stdin[i + 1]);
+				exit (66);
+			}
+			nb_read = 1;
+			while (nb_read)
+			{
+				nb_read = read(fd, buf, 500);
+				dprintf(2, "lecture [%s], nb_read=[%d]\n", cmd[nb]->stdin[i + 1], nb_read);
+				if (nb_read)
+				{
+					dprintf(2, "ecriture: nb_read=[%d], buf=[%s]\n", nb_read, buf);
+					write(fdglobalin, buf, nb_read);
+					buf[0] = '\0';
+				}
+			}
+			close(fd);
+		}
+		else if (ft_strcmp(cmd[nb]->stdin[i], "<<") == 0)
+		{
+			// heredoc
+			// <file1<file2 wc | wc
+		}
+		else
+			exit (66);
+		i += 2;
+	}
+	//dup2(pp[0], 0);
+	//close(pp[0]);
+	close(pp[1]);
+	close(fdglobalin);
+	dprintf(2, "end of ft_redir_in, pp[0]=[%d]\n", pp[0]);
+	return (pp[0]);
+}
+
+int	ft_redir_out(t_cmd **cmd, int nb)
+{
+	int	i;
+	int	fd;
+
+	i = 0;
+	fd = 0;
+	while (cmd[nb]->stdout[i])
+	{
+		if (cmd[nb]->stdout[i + 1] == NULL)
+			exit (67);
+		if (ft_strcmp(cmd[nb]->stdout[i], ">") == 0)
+		{
+			if (fd)
+				close(fd);
+			fd = open(cmd[nb]->stdout[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			dprintf(2, "redir out simple fd=[%d], i=[%d], redir=[%s], file=[%s]\n", fd, i, cmd[nb]->stdout[i], cmd[nb]->stdout[i + 1]);
+			if (fd == -1)
+			{
+				perror(cmd[nb]->stdout[i + 1]);
+			}
+		}
+		else if (ft_strcmp(cmd[nb]->stdout[i], ">>") == 0)
+		{
+			if (fd)
+				close(fd);
+			fd = open(cmd[nb]->stdout[i + 1], O_WRONLY | O_CREAT | O_APPEND, 0644);
+			dprintf(2, "redir out double fd=[%d], i=[%d], redir=[%s], file=[%s]\n", fd, i, cmd[nb]->stdout[i], cmd[nb]->stdout[i + 1]);
+			if (fd == -1)
+			{
+				perror(cmd[nb]->stdout[i + 1]);
+			}
+		}
+		else
+			exit (67);
+		i += 2;
+	}
+	dprintf(2, "end of ft_redir_out, fd=[%d]\n", fd);
+	return (fd);
+}
+
+// gerer les redir out
+void	ft_child(t_cmd **cmd, int **pp, int i)
+{
+	int		fdin;
+	int		fdout;
+
+	if (i == 0)
+		fdin = ft_redir_in2(cmd);
+	else
+	{
+		fdin = pp[i - 1][0];
+	}
+	if (fdin)
+	{
+		dprintf(2, "child[%d] => i=0, dup2(fdin, 0), fdin=[%d]\n", i, fdin);
+		dup2(fdin, 0);
+		close(fdin);
 	}
 	else
 	{
@@ -37,7 +145,8 @@ void	ft_set_pathexec2(t_min *mini, char **cmd)
 {
 	int		fdin;
 	int		fdout;
-
+//	pid_t	pid;
+	
 //	waitpid(0, NULL, 0);
 	if (i == 0)
 		fdin = ft_redir_in2(cmd);
@@ -63,9 +172,7 @@ void	ft_set_pathexec2(t_min *mini, char **cmd)
 		perror("redirection out :");
 		return ;
 	}
-<<<<<<< HEAD
 	ft_exec2(mini, all_path, pathcmd, cmd);
-=======
 	dup2(fdout, 1);
 	if (i != 0)
 
@@ -80,7 +187,7 @@ void	ft_set_pathexec2(t_min *mini, char **cmd)
 //	pid = fork();
 //	if (pid == 0)
 	ft_set_pathexec(&g_mini, cmd[i]->cmd);
-	exit (68);
+	return ;
 }
 
 // main function to manage pipes
