c1c5e98d6409eab229d01184d5dd97b5b38b338f
raphael from linux <rdmrdm3@gmail.com>
2023-02-12 21:40:42 +0100

    echo bug fixed

--- Built-in/echo.c
+++ Built-in/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: clecat <clecat@student.42.fr>              +#+  +:+       +#+        */
+/*   By: rdi-marz <rdi-marz@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 12:37:12 by clecat            #+#    #+#             */
-/*   Updated: 2023/02/10 16:55:49 by clecat           ###   ########.fr       */
+/*   Updated: 2023/02/12 20:07:25 by rdi-marz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,20 @@
 #include <stdlib.h>
 
 //4 fonctions
-//verifie si que des n pour l'option
+//verifie si que des n pour l'option (0) sinon (1)
 int	check_arg_opt(char *arg)
 {
 	int	i;
-	int	y;
 
 	i = 1;
-	y = 1;
+	if (!arg[i])
+		return (1);
 	while (arg[i])
 	{
-		if (arg[i] == 'n')
-			y++;
+		if (arg[i] != 'n')
+			return (1);
 		i++;
 	}
-	if (y != i)
-		return (1);
 	return (0);
 }
 
@@ -84,7 +82,7 @@ int	check_opt(char **arg)
 }
 
 //verifie si echo a des arguments ou non
-void	echo(char **cmd)
+void	echo2(char **cmd)
 {
 	int	i;
 	int	j;
@@ -107,6 +105,36 @@ void	echo(char **cmd)
 			i++;
 		}
 	}
+	g_mini.ret_err = 0;
+	return ;
+}
+
+void	echo(char **cmd)
+{
+	int	i;
+	int	is_option;
+	int	after_option;
+
+	i = 1;
+	is_option = 0;
+	after_option = 0;
+	while (cmd[i])
+	{
+		if (cmd[i][0] == '-' && check_arg_opt(cmd[i]) == 0 && after_option == 0)
+		{
+			is_option = 1;
+		}
+		else
+		{
+			after_option = 1;
+			printf("%s", cmd[i]);
+			if (cmd[i + 1])
+				printf(" ");
+		}
+		i++;	
+	}
+	if (is_option == 0)
+		printf("\n");
 	g_mini.ret_err = 0;
 	return ;
 }
